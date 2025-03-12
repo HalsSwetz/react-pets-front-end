762f7a7d1ca327faf516fdd20a5c8b990895c4c2
Halsey Svetz <halseycheese@gmail.com>
2025-03-12 11:23:33 -0600

    add the crud functionality, need to debug and clean up code

---
+++ src/components/PetForm/PetForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+const PetForm = (props) => {
+  
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    breed: '',
+  });
+
+  
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.handleAddPet(formData);
+    // Right now, if you add a pet and submit the form,
+    // the data entered will stay on the page. We'll fix this soon.
+  };
+
+ 
+  return (
+    <div>
+      <form>
+        <label htmlFor="name"> Name </label>
+        <input
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="age"> Age </label>
+        <input
+          id="age"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="breed"> Breed </label>
+        <input
+          id="breed"
+          name="breed"
+          value={formData.breed}
+          onChange={handleChange}
+        />
+        <button type="submit">Add New Pet</button>
+      </form>
+    </div>
+  );
+};
+
+export default PetForm;
